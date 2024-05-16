e995ac66af230c9225de4683acca3a7ade84516e
kenyansa <kenyansajustine@gmail..com>
2024-05-16 12:27:02 +0300

    use combineReducers to manage state

--- redux.js
+++ redux.js
@@ -1,39 +1,38 @@
-const LOGIN = 'LOGIN'
-const LOGOUT = 'LOGOUT'
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
 
-const defaultState = {
-  authenticated: false
+const counterReducer = (state = 0, action) => {
+  switch(action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
 };
 
-const authReducer = (state = defaultState, action) => {
+const LOGIN = 'LOGIN';
+const LOGOUT = 'LOGOUT';
 
-  switch (action.type) {
-    case LOGIN: 
+const authReducer = (state = {authenticated: false}, action) => {
+  switch(action.type) {
+    case LOGIN:
       return {
         authenticated: true
       }
-    case LOGOUT: 
+    case LOGOUT:
       return {
         authenticated: false
       }
-
     default:
       return state;
-
-  }
-
-};
-
-const store = Redux.createStore(authReducer);
-
-const loginUser = () => {
-  return {
-    type: LOGIN
   }
 };
 
-const logoutUser = () => {
-  return {
-    type: LOGOUT
-  }
-};
+const rootReducer = Redux.combineReducers({
+  count: counterReducer,
+  auth: authReducer
+})
+
+const store = Redux.createStore(rootReducer);
