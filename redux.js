eec5ecf3b02e8be590c989c05108388ace2a790d
kenyansa <kenyansajustine@gmail..com>
2024-05-16 12:46:23 +0300

    send actions with data

--- redux.js
+++ redux.js
@@ -35,4 +35,34 @@ const rootReducer = Redux.combineReducers({
   auth: authReducer
 })
 
-const store = Redux.createStore(rootReducer);
+// const store = Redux.createStore(rootReducer);
+
+
+//case 2
+const ADD_NOTE = 'ADD_NOTE';
+
+const notesReducer = (state = 'Initial State', action) => {
+  switch(action.type) {
+    // Change code below this line
+    case ADD_NOTE:
+        return action.text;
+    // Change code above this line
+    default:
+      return state;
+  }
+};
+
+const addNoteText = (note) => {
+  // Change code below this line
+  return {
+    type: ADD_NOTE,
+    text: note
+  }
+  // Change code above this line
+};
+
+const store = Redux.createStore(notesReducer);
+
+console.log(store.getState());
+store.dispatch(addNoteText('Hello!'));
+console.log(store.getState());
